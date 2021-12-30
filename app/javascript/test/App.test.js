14fc13b26eefd384a317a4894b4734fa28516653
ChrisGadek1 <krzysztof.gadek15@gmail.com>
2021-12-31 00:04:47 +0100

    set up jest-enzyme test for react

---
+++ app/javascript/test/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from "../components/App";
+import {shallow} from 'enzyme'
+
+describe("App", () => {
+    it('renders App component without crash', () => {
+        const wrapper = shallow(<App />)
+
+        expect(wrapper.text()).toBe("Hello World")
+    });
+});
