9b241abe12dabd7fef47f683f789c6575bb4e183
ChrisGadek1 <krzysztof.gadek15@gmail.com>
2022-01-13 01:20:49 +0100

    added footer

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Nav from "./nav/Nav";
 import Main from "./main/Main";
+import Footer from "./footer/Footer";
 
 const App = () => (
-    <div style={{backgroundColor: "#84a4c2", minHeight: "100vh"}}>
-        <Nav/>
-        <Main />
+    <div className="d-flex flex-column" style={{backgroundColor: "#84a4c2", minHeight: "100vh"}}>
+        <div className="page-content">
+            <Nav/>
+            <Main />
+        </div>
+        <Footer />
     </div>
 )
 
