dc0adc9de8bb02a7cfebd8ff2fa324b4805af61e
ChrisGadek1 <krzysztof.gadek15@gmail.com>
2022-02-22 02:05:15 +0100

    working main site button

--- app/javascript/components/nav/NavMobile.js
+++ app/javascript/components/nav/NavMobile.js
@@ -1,8 +1,9 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const NavMobile = () => (
     <>
-        <a className="navbar-brand" href="#">Messenger App</a>
+        <Link className="navbar-brand" to={"/"}>Messenger App</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                 aria-expanded="false" aria-label="Toggle navigation">
