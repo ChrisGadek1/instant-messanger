917cec0875d972a0c5ca6b434415317b94b68204
ChrisGadek1 <krzysztof.gadek15@gmail.com>
2022-02-23 01:01:09 +0100

    shows avatar in the navbar

--- app/javascript/components/nav/NavMobile.js
+++ app/javascript/components/nav/NavMobile.js
@@ -1,15 +1,30 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
 
-const NavMobile = () => (
-    <>
-        <Link className="navbar-brand" to={"/"}>Messenger App</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-    </>
-)
+const NavMobile = () => {
+
+    const user = useSelector(store => store.user);
+
+    return(
+        <>
+            <div>
+                { user !== null && user.name !== undefined ?
+                    <Link
+                        className = "navbar-brand"
+                        to = {"/"} >
+                        <img src={user.avatar} width="35px" height="35px" />
+                    < /Link> : null }
+                <Link className="navbar-brand" to={"/"}>Messenger App</Link>
+            </div>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+        </>
+        )
+
+}
 
 export default NavMobile;
